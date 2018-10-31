13d7a2c6a7877dea112e1b12c306fe459d0bbc89
taylorbailey97 <36019252+taylorbailey97@users.noreply.github.com>
2018-10-31 17:08:37 -0600

    uselessFunction added

--- app/src/main/java/com/example/brendon/teach07/MainActivity.java
+++ app/src/main/java/com/example/brendon/teach07/MainActivity.java
@@ -10,4 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void uselessFunction() {
+        //This is a useless function
+    }
 }
